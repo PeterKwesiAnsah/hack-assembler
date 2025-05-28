9200b245b21f3153b3c4014c193c1e4770eb17d2
peterkwesiansah <ansahpeter123@yahoo.com>
2025-05-28 19:55:57 +0000

    feat: lexer looking good

--- lexical-scanner.c
+++ lexical-scanner.c
@@ -1,19 +1,96 @@
 #include "lexical-scanner.h"
+#include "arena.h"
+#include <sys/_types/_null.h>
+
 
 
 ScannerTokens tkns;
-int line,clabel=1;
+int line,clabel=0;
 ScannerTokens scanner(char *source,int len){
+    struct Arena *amemory=create_arena(1024);
     int start,current=0;
     while(current<len){
         start=current;
         char c=source[current];
         current++;
         switch (c) {
-         case '@':
-            break;
-         default:
-            break;
+        case '@':
+            //Ains
+            for(;(source[current]!='\n');current++)
+            //create A ins token
+            //append to slice
+        break;
+        case '(':
+            //Ldec
+            while((source[current++]!=')'))
+            //create Label dec token
+         break;
+         case '/':
+             //comment
+            for(;(source[current]!='\n');current++)
+        break;
+       	case ' ':
+          break;
+		case '\r':
+          break;
+		case '\t':
+		  break;
+        case '\n':
+            ++line;
+        break;
+        default:{
+            //find dest,jump operators
+            char *destOp =NULL;
+            char *jumpOp=NULL;
+            char charcur;
+            for(;(charcur=source[current],charcur!='\n');current++){
+                if(charcur=='='){
+                    destOp=source + current;
+                }else if (charcur==';') {
+                    destOp=source + current;
+                }
+            }
+            if(destOp){
+              //create dest token
+              struct token *tptr=(struct token *)aalloc(amemory, sizeof(struct token));
+              tptr->tt=S_DEST;
+              tptr->lexeme=&source[start];
+              tptr->len=destOp-&source[start];
+              //append to slice
+              {
+                  //create comp token
+                  struct token *tptr=(struct token *)aalloc(amemory, sizeof(struct token));
+                  tptr->tt=S_COMP;
+                  tptr->lexeme=destOp+1;
+                  char *end=jumpOp ? jumpOp: &source[current];
+                  tptr->len=end-destOp+1;
+                  //append to slice
+              }
+            }else {
+                //create dest token (null)
+                struct token *tptr=(struct token *)aalloc(amemory, sizeof(struct token));
+                tptr->tt=S_DEST;
+                tptr->lexeme=NULL;
+                tptr->len=0;
+                //append to slice
+            }
+            if(jumpOp){
+                //create jump0p
+                struct token *tptr=(struct token *)aalloc(amemory, sizeof(struct token));
+                tptr->tt=S_JMP;
+                tptr->lexeme=jumpOp+1;
+                tptr->len=&source[current]-jumpOp+1;
+                //append to slice
+            }else {
+                //create jump token (null)
+                struct token *tptr=(struct token *)aalloc(amemory, sizeof(struct token));
+                tptr->tt=S_JMP;
+                tptr->lexeme=NULL;
+                tptr->len=0;
+                //append to slice
+            }
+           break;
+        }
         }
     }
     return tkns;
--- lexical-scanner.h
+++ lexical-scanner.h
@@ -1,12 +1,14 @@
 #ifndef LEX
 #define LEX
 #include "darray.h"
-enum tokenType {COMP,DEST,JMP,LABEL,A};
+enum tokenType {S_COMP,S_DEST,S_JMP,S_LABEL,S_A};
 struct token {
     enum tokenType tt;
+    //cannot be used as a string but as a character array.
     char *lexeme;
+    int len;
 };
 //create tokens slice type
-slice(struct token, ScannerTokens)
+slice(struct token *, ScannerTokens)
 ScannerTokens scanner(char *source,int len);
 #endif
