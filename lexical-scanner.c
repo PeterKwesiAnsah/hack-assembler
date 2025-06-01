6e1d4758c8a6f47408a595c16625174d6ec6c3e7
peterkwesiansah <ansahpeter123@yahoo.com>
2025-06-01 16:19:54 +0000

    feat: scanner complete

--- lexical-scanner.c
+++ lexical-scanner.c
@@ -1,6 +1,9 @@
 #include "lexical-scanner.h"
 #include "arena.h"
 #include "darray.h"
+#include <stddef.h>
+#include <stdint.h>
+#include <memory.h>
 
 
 
@@ -18,11 +21,20 @@ ScannerTokens scanner(char *source,int len){
             clabel++;
             char isID=!isdigit(source[current]);
             for(;(source[current]!='\n');current++);
+            const size_t lexemeLen =(&source[current])-((&source[start])+1);
             //create ID or Number token
-            struct token *tptr=(struct token *)aalloc(amemory, sizeof(struct token));
+            // +1 for null character
+            struct token *tptr=(struct token *)aalloc(amemory, sizeof(struct token)+(lexemeLen+1));
             tptr->tt=isID ? S_ID:S_NUMBER;
-            tptr->lexeme=(&source[start])+1;
-            tptr->len=(&source[current])-((&source[start])+1);
+            tptr->lexeme = ((char *)tptr) + sizeof(struct token);
+
+            //copy from source
+            memcpy((((char *)tptr) + sizeof(struct token)),(&source[start])+1, lexemeLen);
+            //null character
+            tptr->lexeme[lexemeLen]='\0';
+            tptr->len=lexemeLen;
+
+            //tptr->lexeme=(&source[start])+1;
             //append to slice
             append(tkns, tptr, (sizeof(struct token *)));
             //TODO: if token is ID we insert into a symbol tabel
@@ -38,12 +50,14 @@ ScannerTokens scanner(char *source,int len){
             //line comment
             if(nextchar=='/'){
                 for(;(source[current]!='\n');current++);
-                continue;
+                break;
             }
             //single block comment
             //if(nextchar!='*')//wrong syntax, we don't worry about syntax error for now.
 
             // skip over '*'
+            current++;
+
             while((source[current++]!='*'));
             //skip over '/'
             current++;
@@ -57,7 +71,7 @@ ScannerTokens scanner(char *source,int len){
 		  break;
         case '\n':
             ++line;
-        break;
+         break;
         default:{
             //find dest,jump operators
             char *destOp =NULL;
@@ -67,51 +81,99 @@ ScannerTokens scanner(char *source,int len){
                 if(charcur=='='){
                     destOp=source + current;
                 }else if (charcur==';') {
-                    destOp=source + current;
+                    jumpOp=source + current;
                 }
             }
             if(destOp){
               //create dest token
-              struct token *tptr=(struct token *)aalloc(amemory, sizeof(struct token));
+              const size_t lexemeLen=destOp-(&source[start]);
+              //+1 for null character
+              struct token *tptr=(struct token *)aalloc(amemory, sizeof(struct token)+(lexemeLen+1));
               tptr->tt=S_DEST;
-              tptr->lexeme=&source[start];
-              tptr->len=destOp-(&source[start]);
+              tptr->lexeme = ((char *)tptr) + sizeof(struct token);
+
+              //copy from source
+              memcpy((((char *)tptr) + sizeof(struct token)),(&source[start]), lexemeLen);
+              //null character
+              tptr->lexeme[lexemeLen]='\0';
+              tptr->len=lexemeLen;
+
+              //tptr->lexeme=&source[start];
               //append to slice
               append(tkns, tptr, (sizeof(struct token *)));
               {
                   //create comp token
-                  struct token *tptr=(struct token *)aalloc(amemory, sizeof(struct token));
-                  tptr->tt=S_COMP;
-                  tptr->lexeme=destOp+1;
                   char *end=jumpOp ? jumpOp: &source[current];
+                  const size_t lexemeLen=end-(destOp+1);
+                  //+1 for null character
+                  struct token *tptr=(struct token *)aalloc(amemory, sizeof(struct token)+(lexemeLen+1));
+                  tptr->tt=S_COMP;
+                  tptr->lexeme = ((char *)tptr) + sizeof(struct token);
                   tptr->len=end-(destOp+1);
+
+                  //copy from source
+                  memcpy((((char *)tptr) + sizeof(struct token)),(destOp+1), lexemeLen);
+                  //null character
+                  tptr->lexeme[lexemeLen]='\0';
+
+                  //tptr->lexeme=destOp+1;
                   //append to slice
                   append(tkns, tptr, (sizeof(struct token *)));
               }
             }else {
                 clabel++;
                 //create dest token (null)
-                struct token *tptr=(struct token *)aalloc(amemory, sizeof(struct token));
+                struct token *tptr=(struct token *)aalloc(amemory, sizeof(struct token)+1);
                 tptr->tt=S_DEST;
-                tptr->lexeme=NULL;
+                tptr->lexeme=((char *)tptr) + sizeof(struct token);
+                tptr->lexeme[0]='\0';
                 tptr->len=0;
                 //append to slice
                 append(tkns, tptr, (sizeof(struct token *)));
             }
             if(jumpOp){
+                //TODO: refactor as CREATE_COMP macro
+                if(!destOp){
+                    //create comp token
+                    char *end=jumpOp;
+                    const size_t lexemeLen=end-(&source[start]);
+                    //+1 for null character
+                    struct token *tptr=(struct token *)aalloc(amemory, sizeof(struct token)+(lexemeLen+1));
+                    tptr->tt=S_COMP;
+                    tptr->lexeme = ((char *)tptr) + sizeof(struct token);
+                    tptr->len=end-(&source[start]);
+
+                    //copy from source
+                    memcpy((((char *)tptr) + sizeof(struct token)),(&source[start]), lexemeLen);
+                    //null character
+                    tptr->lexeme[lexemeLen]='\0';
+
+                    //tptr->lexeme=&source[start];
+                    //append to slice
+                    append(tkns, tptr, (sizeof(struct token *)));
+                }
                 //create jump0p
-                struct token *tptr=(struct token *)aalloc(amemory, sizeof(struct token));
+                const size_t lexemeLen=&source[current]-(jumpOp+1);
+                struct token *tptr=(struct token *)aalloc(amemory, sizeof(struct token)+(lexemeLen + 1));
                 tptr->tt=S_JMP;
-                tptr->lexeme=jumpOp+1;
+                tptr->lexeme = ((char *)tptr) + sizeof(struct token);
                 tptr->len=&source[current]-(jumpOp+1);
+
+                //copy from source
+                memcpy((((char *)tptr) + sizeof(struct token)),(jumpOp+1), lexemeLen);
+                //null character
+                tptr->lexeme[lexemeLen]='\0';
+
+
+                //tptr->lexeme=jumpOp+1;
                 //append to tokens slice
                 append(tkns, tptr, (sizeof(struct token *)));
-                //TODO: create Comp
             }else {
                 //create jump token (null)
-                struct token *tptr=(struct token *)aalloc(amemory, sizeof(struct token));
+                struct token *tptr=(struct token *)aalloc(amemory, sizeof(struct token)+1);
                 tptr->tt=S_JMP;
-                tptr->lexeme=NULL;
+                tptr->lexeme=((char *)tptr) + sizeof(struct token);
+                tptr->lexeme[0]='\0';
                 tptr->len=0;
                 //append to tokens slice
                 append(tkns, tptr, (sizeof(struct token *)));
@@ -119,6 +181,6 @@ ScannerTokens scanner(char *source,int len){
            break;
         }
     }
-    }
+}
     return tkns;
 }
