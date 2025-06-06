b1c9112d179f353ea27ff3446e06bf971d60ef67
peterkwesiansah <ansahpeter123@yahoo.com>
2025-06-06 20:03:45 +0000

    feat: code generation

---
+++ code-gen.c
@@ -0,0 +1,90 @@
+#include "code-gen.h"
+#include "lexical-scanner.h"
+#include "symbolTable.h"
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+
+typedef struct {
+    char* key;
+    char* value;
+} KeyValuePair;
+
+KeyValuePair comp_table[] = {
+    {"0", "101010"},    {"1", "111111"},    {"-1", "111010"},   {"D", "001100"},
+    {"A", "110000"},    {"!D", "001101"},   {"!A", "110001"},   {"-D", "001111"},
+    {"-A", "110011"},   {"D+1", "011111"},  {"A+1", "110111"},  {"D-1", "001110"},
+    {"A-1", "110010"},  {"D+A", "000010"},  {"D-A", "010011"},  {"A-D", "000111"},
+    {"D&A", "000000"},  {"D|A", "010101"}
+};
+
+KeyValuePair dest_table[] = {
+    {"", "000"}, {"M", "001"},    {"D", "010"},    {"MD", "011"},
+    {"A", "100"},    {"AM", "101"},   {"AD", "110"},   {"AMD", "111"}
+};
+
+KeyValuePair jump_table[] = {
+    {"", "000"}, {"JGT", "001"},  {"JEQ", "010"},  {"JGE", "011"},
+    {"JLT", "100"},  {"JNE", "101"},  {"JLE", "110"},  {"JMP", "111"}
+};
+
+void fgenerate(ScannerTokens tokens, FILE * f){
+int i=0;
+while(i<tokens.len){
+    enum tokenType curtt=tokens.arr[i]->tt;
+    char hackMachineCode[HACK_MACHINE_WORD+1];
+    hackMachineCode[HACK_MACHINE_WORD]='\0';
+    if(curtt==S_ID || curtt==S_NUMBER){
+        char *p_end = NULL;
+        hackMachineCode[0]='0';
+        unsigned long strInt;
+        //a ins
+        if(curtt==S_NUMBER){
+            //translate directly
+            strInt = strtoul(tokens.arr[i]->lexeme, &p_end, 10);
+        }else{
+            //lookup id in table
+            struct KVN *trow=getSymValue(tokens.arr[i]->lexeme);
+            assert(trow!=NULL);
+            strInt = trow->value;
+        }
+        int startIndex=HACK_MACHINE_WORD-1,endIndex=1,shiftPlaces=0;
+        for(; startIndex>=endIndex;(startIndex--,shiftPlaces++)){
+            hackMachineCode[startIndex]=((strInt & 1<<shiftPlaces) > 0) ? '1':'0';
+        }
+        fputs(hackMachineCode, f);
+        i++;
+    }else {
+        //c ins
+        char *dest=NULL;
+        char *comp=NULL;
+        char *jmp=NULL;
+        hackMachineCode[0]='1';
+        hackMachineCode[1]='1';
+        hackMachineCode[2]='1';
+        hackMachineCode[3]='0';
+
+        int in=0;
+        for(;in<COMP_TABLE_SIZE && !(dest && comp && jmp);in++){
+            assert(tokens.arr[i]->tt==S_DEST);
+            dest=(strcmp(dest_table[in].key,tokens.arr[i]->lexeme)==0) ? dest_table[in].value : NULL;
+            assert(tokens.arr[i+1]->tt==S_COMP);
+            char *searchChar;
+             if((searchChar=strchr(tokens.arr[i+1]->lexeme,'M'))){
+               //replace M with A
+               *searchChar='A';
+               hackMachineCode[3]='1';
+           }
+            comp=(strcmp(comp_table[in].key,tokens.arr[i+1]->lexeme)==0) ? comp_table[in].value : NULL;
+            assert(tokens.arr[i+2]->tt==S_JMP);
+            jmp=(strcmp(jump_table[in].key,tokens.arr[i+2]->lexeme)==0) ? jump_table[in].value : NULL;
+        }
+        i=i+3;
+        memcpy(hackMachineCode+DEST_HACK_START_INDEX,dest, DEST_HACK_LEN);
+        memcpy(hackMachineCode+COMP_HACK_START_INDEX,comp, COMP_HACK_LEN);
+        memcpy(hackMachineCode+JUMP_HACK_START_INDEX,jmp , JUMP_HACK_LEN);
+        fputs(hackMachineCode, f);
+    }
+}
+}
---
+++ code-gen.h
@@ -0,0 +1,20 @@
+//produce code from stream of tokens
+#ifndef CODE_GEN
+#include "lexical-scanner.h"
+#include <stdio.h>
+#define HACK_MACHINE_WORD 16
+#include <string.h>
+#define COMP_HACK_LEN 6
+#define DEST_HACK_LEN 3
+#define JUMP_HACK_LEN 3
+
+#define COMP_HACK_START_INDEX 4
+#define DEST_HACK_START_INDEX 10
+#define JUMP_HACK_START_INDEX 13
+
+#define COMP_TABLE_SIZE 18
+#define DEST_TABLE_SIZE 8
+#define JUMP_TABLE_SIZE 8
+#define CODE_GEN
+void fgenerate(ScannerTokens, FILE * );
+#endif
