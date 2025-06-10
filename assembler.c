eec74a7d6f2de4fe67cc115531f28c08eba18c70
peterkwesiansah <ansahpeter123@yahoo.com>
2025-06-10 13:28:33 +0000

    feat: read from program source code from file

--- assembler.c
+++ assembler.c
@@ -3,46 +3,16 @@
 #include <string.h>
 #include "darray.h"
 #include "lexical-scanner.h"
-
+#include "code-gen.h"
 
 
 int main(){
-    //testing A(number) and C ins (with dest)
-    char *hack_program =
-    "@2\n"
-    "D=A\n"
-    "@3\n"
-    "D=D+A\n"
-    "@0\n"
-    "M=D\n";
-    ScannerTokens programResults=scanner(hack_program, strlen(hack_program));
-    struct token *expectedTkns[] = {
-        &(struct token){S_NUMBER, "2", 1},
-        &(struct token){S_DEST, "D", 1},
-        &(struct token){S_COMP, "A", 1},
-        &(struct token){S_JMP, NULL, 0},
-        &(struct token){S_NUMBER, "3", 1},
-        &(struct token){S_DEST, "D", 1},
-        &(struct token){S_COMP, "D+A", 3},
-        &(struct token){S_JMP, NULL, 0},
-        &(struct token){S_NUMBER, "0", 1},
-        &(struct token){S_DEST, "M", 1},
-        &(struct token){S_COMP, "D", 1},
-        &(struct token){S_JMP, NULL, 0},
-    };
-    int array_size=(sizeof(expectedTkns)/sizeof(expectedTkns[0]));
-    ScannerTokens expectedResults={DEFAULT_SLICE_CAP,array_size};
-    expectedResults.arr=expectedTkns;
-    //assert slice lengths
-    assert(programResults.len==expectedResults.len);
-    //assert individual tokens
-    for(int i=0;i<array_size;i++){
-        //assert token members (token type)
-        assert(expectedResults.arr[i]->tt==programResults.arr[i]->tt);
-        //assert token members (token lexeme)
-        //
-        // assert tokem members (len)
-        assert(expectedResults.arr[i]->len==programResults.arr[i]->len);
-    }
+    fseek(stdin, 0, SEEK_END);
+    long size = ftell(stdin);
+    rewind(stdin);
+    char source[size];
+    fread(source,size, 1, stdin);
+    ScannerTokens tkns=scanner(source, size);
+    fgenerate(tkns, stdout);
     return 0;
 }
