d5717bd2cfa121cef09779f95a41bda7a1a302ae
peterkwesiansah <ansahpeter123@yahoo.com>
2025-09-16 08:52:15 +0000

    remove stdin redirection

--- assembler.c
+++ assembler.c
@@ -1,14 +1,20 @@
+#include <_stdio.h>
 #include <stdio.h>
 #include "lexical-scanner.h"
 #include "code-gen.h"
 
 
-int main(){
-     fseek(stdin, 0, SEEK_END);
-     long size = ftell(stdin);
-     rewind(stdin);
+int main(int argc, char *argv[]){
+    FILE *fsource=fopen(argv[1], "r");
+    if (!fsource){
+        fputs("Cannot open source file", stderr);
+        return 1;
+    }
+     fseek(fsource, 0, SEEK_END);
+     long size = ftell(fsource);
+     rewind(fsource);
      char source[size];
-     fread(source,size, 1, stdin);
+     fread(source,size, 1, fsource);
      ScannerTokens tkns=scanner(source, size);
     fgenerate(tkns);
     return 0;
