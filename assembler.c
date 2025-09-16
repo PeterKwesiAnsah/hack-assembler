4ad8e2a9b7368e829a4348aa73c7b7c027839ed0
peterkwesiansah <ansahpeter123@yahoo.com>
2025-09-16 09:21:08 +0000

    add program usage

--- assembler.c
+++ assembler.c
@@ -4,6 +4,10 @@
 
 
 int main(int argc, char *argv[]){
+    if (argc!=2) {
+        fputs("Usage: assembler [sourcefile] >[outputfile]\n",stderr);
+        return 1;
+    }
     FILE *fsource=fopen(argv[1],"r");
     if (!fsource){
         fputs("Cannot open source file",stderr);
